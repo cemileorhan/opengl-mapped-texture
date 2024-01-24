65eb1b2f697bf79aa1bce7bd6b8143819d581123
Cemile Orhan <cemiileorhan@gmail.com>
2024-01-24 16:44:26 +0300

    opengl-mapped-texture
    mapped texture using OpenGL in computer graphics

---
+++ mapped-texture/ders10/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ders10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(2, 12);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(788, 470);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 0;
+            this.simpleOpenGlControl1.Load += new System.EventHandler(this.simpleOpenGlControl1_Load);
+            this.simpleOpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.simpleOpenGlControl1_Paint);
+            this.simpleOpenGlControl1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.simpleOpenGlControl1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1025, 514);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl OpenGLC;
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+    }
+}
+
---
+++ mapped-texture/ders10/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+
+namespace ders10
+{
+    public partial class Form1 : Form
+    {
+        float alpha = 0, beta = 0;
+        int bwidth, bheight;
+        Bitmap curImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+            simpleOpenGlControl1.InitializeContexts();
+            reshape();
+        }
+        void reshape()
+        {
+            var filename = "brick1.bmp";
+            var bmp = new Bitmap(filename);
+            bwidth = bmp.Width;
+            bheight = bmp.Height;
+            var bmpData = bmp.LockBits(
+                new Rectangle(0, 0, bmp.Width, bmp.Height),
+                ImageLockMode.ReadOnly,
+                PixelFormat.Format24bppRgb);
+            Gl.glViewport(0, 0, bwidth, bheight);
+            Gl.glClearColor(0.1f, 0.39f, 0.88f, 1);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluPerspective(60, 2, 1, 50);
+
+            //60 degree specifies the field of view angle
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Glu.gluLookAt(2, -1, 5, 0, 0, 0, 0, 1, 0);
+            Gl.glLoadIdentity();
+            Gl.glEnable(Gl.GL_TEXTURE_2D); //Doku Açma
+            Gl.glTexImage2D(
+                              Gl.GL_TEXTURE_2D,
+                              0, // Level 0 level for MipMap(LOD)
+                              Gl.GL_RGBA, // use R,G,B and Alpha components
+                              bmp.Width, bmp.Height, // Texture width and height size
+                              0, // border width
+                              Gl.GL_BGR_EXT, //texels are in RGB format
+                              Gl.GL_UNSIGNED_BYTE,
+                              bmpData.Scan0
+                              );
+            Gl.glBindTexture(Gl.GL_TEXTURE_2D, 0); // bind a named texture to a texturing target
+            Gl.glTexParameterf(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MIN_FILTER, Gl.GL_LINEAR); // adjust texel to the nearest pixel center 
+            Gl.glTexParameterf(Gl.GL_TEXTURE_2D, Gl.GL_TEXTURE_MAG_FILTER, Gl.GL_NEAREST);
+
+        }
+        private void simpleOpenGlControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleOpenGlControl1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.D)
+                alpha += 5;
+            if (e.KeyCode == Keys.A)
+                beta += 5;
+            if (e.KeyCode == Keys.S)
+                alpha -= 5;
+            if (e.KeyCode == Keys.W)
+                beta -= 5;
+            simpleOpenGlControl1.Refresh();
+        }
+
+        private void simpleOpenGlControl1_Paint(object sender, PaintEventArgs e)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glPushMatrix();
+            Gl.glTranslatef(0, 0, -3);
+            Gl.glRotatef(alpha, 1, 0, 0);
+            Gl.glRotatef(beta, 0, 1, 0);
+            display();
+            Gl.glPopMatrix();
+        }
+        void display()
+        {
+
+            Gl.glBegin(Gl.GL_TRIANGLES);
+            //left triangle
+            Gl.glTexCoord2f(0.5f, 1f); Gl.glVertex2f(-3, 3);
+            Gl.glTexCoord2f(0, 0); Gl.glVertex2f(-3, 0);
+            Gl.glTexCoord2f(1, 0); Gl.glVertex2f(0, 0);
+            //right triangle
+            Gl.glTexCoord2f(4, 8); Gl.glVertex2f(3, 3);
+            Gl.glTexCoord2f(0, 0); Gl.glVertex2f(0, 0);
+            Gl.glTexCoord2f(8, 0); Gl.glVertex2f(3, 0);
+            //bottom
+            Gl.glTexCoord2f(1, 0.4f); Gl.glVertex2f(0, 0);
+            Gl.glTexCoord2f(0.8f, 1); Gl.glVertex2f(-1.5f, -3);
+            Gl.glTexCoord2f(0.3f, 0.4f); Gl.glVertex2f(1.5f, -3);
+
+            Gl.glEnd();
+        }
+
+    }
+}
